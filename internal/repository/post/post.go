5861e4605af12d65c385e61bbc20f5f465d344d1
Sarvarov Mustafa <sarvarov.mustafa@gmail.com>
2024-02-11 02:05:21 +0600

    refactor: merge 2 functions in tag repo into 1

--- internal/repository/post/post.go
+++ internal/repository/post/post.go
@@ -93,18 +93,8 @@ func (r *postRepository) GetPost(postID int) (entity.PostEntity, error) {
 	return post, nil
 }
 
-func (r *postRepository) GetAllPosts() (*[]entity.PostEntity, error) {
-	query := `
-		SELECT p.id, p.title, p.content, p.created_at, u.username, 
-			SUM(CASE WHEN pr.is_like = true THEN 1 ELSE 0 END) as likes_count,
-			SUM(CASE WHEN pr.is_like = false THEN 1 ELSE 0 END) as dislikes_count
-		FROM posts p
-		INNER JOIN users u ON p.user_id = u.id
-		LEFT JOIN post_reactions pr ON p.id = pr.post_id
-		GROUP BY p.id
-	`
-
-	rows, err := r.DB.Query(query)
+func (r *postRepository) getAllPostsByQuery(query string, args ...interface{}) (*[]entity.PostEntity, error) {
+	rows, err := r.DB.Query(query, args...)
 	if err != nil {
 		return nil, err
 	}
@@ -128,6 +118,20 @@ func (r *postRepository) GetAllPosts() (*[]entity.PostEntity, error) {
 	return &posts, nil
 }
 
+func (r *postRepository) GetAllPosts() (*[]entity.PostEntity, error) {
+	query := `
+		SELECT p.id, p.title, p.content, p.created_at, u.username, 
+			SUM(CASE WHEN pr.is_like = true THEN 1 ELSE 0 END) as likes_count,
+			SUM(CASE WHEN pr.is_like = false THEN 1 ELSE 0 END) as dislikes_count
+		FROM posts p
+		INNER JOIN users u ON p.user_id = u.id
+		LEFT JOIN post_reactions pr ON p.id = pr.post_id
+		GROUP BY p.id
+	`
+
+	return r.getAllPostsByQuery(query)
+}
+
 func (r *postRepository) GetAllPostsByTagId(tagID int) (*[]entity.PostEntity, error) {
 	query := `
 		SELECT p.id, p.title, p.content, p.created_at, u.username, 
@@ -144,26 +148,5 @@ func (r *postRepository) GetAllPostsByTagId(tagID int) (*[]entity.PostEntity, er
 		GROUP BY p.id
 	`
 
-	rows, err := r.DB.Query(query, tagID)
-	if err != nil {
-		return nil, err
-	}
-
-	var posts []entity.PostEntity
-
-	for rows.Next() {
-		var post entity.PostEntity
-		if err := rows.Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt,
-			&post.Username, &post.Likes, &post.Dislikes); err != nil {
-
-			return nil, err
-		}
-		posts = append(posts, post)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return &posts, nil
+	return r.getAllPostsByQuery(query, tagID)
 }
